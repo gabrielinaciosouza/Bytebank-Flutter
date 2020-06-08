b2223172c5607a2ea9285be72f1295c9292aba3e
Gabriel Inacio <gabriel.sert9@gmail.com>
2020-06-08 17:33:25 -0300

    Projeto Finalizado

---
+++ lib/models/tranferencia.dart
@@ -0,0 +1,11 @@
+class Transferencia {
+  double valor;
+  int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+
+  @override
+  String toString() {
+    return 'Transferencia{valor: $valor, numeroConta: $numeroConta}';
+  }
+}
